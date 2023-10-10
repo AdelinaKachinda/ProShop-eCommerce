66ce981213ad55aa60492ba6bc9b9943b5ec7a4f
Adelina Kachinda <adeli@Adelinas-MacBook-Pro.local>
2023-10-10 15:25:36 -0400

    added footer, header and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap";
+
+
 const App = () => {
   return <div>
-    <h1>Welcome!</h1>
+    <>
+    <Header/>
+    <main className="py-3">
+    <Container>
+        <h1>Welcome to ProShop!</h1>
+    </Container>
+    </main>
+    <Footer/>
+    </>
+    
   </div>
 }
 
