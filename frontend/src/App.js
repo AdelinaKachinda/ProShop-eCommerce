dea764858505e927299252f0bd2195da353b8694
Adelina Kachinda <adeli@Adelinas-MacBook-Pro.local>
2023-10-13 17:41:36 -0400

    added react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 
 const App = () => {
@@ -9,9 +10,10 @@ const App = () => {
     <Header/>
     <main className="py-3">
     <Container>
-        <h1>Welcome to ProShop!</h1>
+        <Outlet />
     </Container>
     </main>
+    
     <Footer/>
     </>
     
