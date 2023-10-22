32f20573f8d4d61d5a4c795e3293e2775ec19f28
Adelina Kachinda <adeli@Adelinas-MacBook-Pro.local>
2023-10-22 17:32:07 -0400

    completing hard-coded frontend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,8 +6,11 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Latest products</h1>
+
             <Row>
+
             {products.map((product) => (
+                
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
                 </Col>
